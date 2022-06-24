69d1f30ecfff96e84bef627d588a5a85d09da37b
Emre Özgenç <emreozgenc@hotmail.com.tr>
2022-06-25 01:13:48 +0300

    Add UBL_TR API endpoint

--- src/main/java/com/nilvera/xmlvalidationjavalin/Application.java
+++ src/main/java/com/nilvera/xmlvalidationjavalin/Application.java
@@ -1,10 +1,18 @@
 package com.nilvera.xmlvalidationjavalin;
 
+import com.nilvera.xmlvalidationjavalin.constants.Path;
+import com.nilvera.xmlvalidationjavalin.models.XmlValidationModel;
+import com.nilvera.xmlvalidationjavalin.validators.UblTrValidator;
 import io.javalin.Javalin;
 
 public class Application {
+    private static final int PORT = 8181;
     public static void main(String[] args) {
-        Javalin app = Javalin.start(7000);
-        app.get("/", ctx -> ctx.result("Hello World"));
+        Javalin app = Javalin.start(PORT);
+
+        app.post(Path.UBL_TR, ctx -> {
+            XmlValidationModel model = new UblTrValidator().validate(ctx.uploadedFile("xmlFile").getContent());
+            ctx.json(model);
+        });
     }
 }
