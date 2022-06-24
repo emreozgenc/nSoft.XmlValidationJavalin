18f091efb31a89b096c86e16ff4f5b8bb5253bca
Emre Özgenç <emreozgenc@hotmail.com.tr>
2022-06-25 01:11:12 +0300

    Add Path constant class

---
+++ src/main/java/com/nilvera/xmlvalidationjavalin/constants/Path.java
@@ -0,0 +1,6 @@
+package com.nilvera.xmlvalidationjavalin.constants;
+
+public class Path {
+    private static final String DEFAULT = "/validate/schematron";
+    public static final String UBL_TR = DEFAULT + "/ubltr_main";
+}
