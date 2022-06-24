036f2a3ff7ecc7928f12d5f1818222070b2ae2d5
Emre Özgenç <emreozgenc@hotmail.com.tr>
2022-06-25 01:12:18 +0300

    Add Validator interface

---
+++ src/main/java/com/nilvera/xmlvalidationjavalin/validators/Validator.java
@@ -0,0 +1,9 @@
+package com.nilvera.xmlvalidationjavalin.validators;
+
+import com.nilvera.xmlvalidationjavalin.models.XmlValidationModel;
+
+import java.io.InputStream;
+
+public interface Validator {
+    XmlValidationModel validate(InputStream inputStream);
+}
