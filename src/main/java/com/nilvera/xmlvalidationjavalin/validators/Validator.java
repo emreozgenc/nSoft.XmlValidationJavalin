3bb8ebf8d7c597f8380d5989c1065ab06e1060eb
Emre Özgenç <emreozgenc@hotmail.com.tr>
2022-06-27 09:42:41 +0300

    Add throw exception sign to validate method

--- src/main/java/com/nilvera/xmlvalidationjavalin/validators/Validator.java
+++ src/main/java/com/nilvera/xmlvalidationjavalin/validators/Validator.java
@@ -5,5 +5,5 @@ import com.nilvera.xmlvalidationjavalin.models.XmlValidationModel;
 import java.io.InputStream;
 
 public interface Validator {
-    XmlValidationModel validate(InputStream inputStream);
+    XmlValidationModel validate(InputStream inputStream) throws Exception;
 }
